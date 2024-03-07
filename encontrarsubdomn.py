7963bef41bbfd6d2847de8516ad3a712b40f083b
Gabrieltxtkl <162597726+Gabrieltxtkl@users.noreply.github.com>
2024-03-07 10:36:58 -0400

    Add files via upload

---
+++ encontrarsubdomn.py
@@ -0,0 +1,35 @@
+import random
+
+words = []
+counter = 0
+answ = "s"
+word = 0
+
+while answ == "s":
+    answ = str.lower(input("Você quer gerar uma WordList?(S/N): "))
+    if answ != "s":
+        print("Você não quer fazer uma wordlist e o script parou")
+        break
+    print("Para parar de entrar palavras digite -0")
+    while word != "-0":
+        counter += 1
+        word = input("Digite uma palavra: ")
+        if word == "-0":
+            print("Você escolheu parar de entrar palavras")
+            break
+        else:
+            words.append(word)
+    senhas = int(input("Quantas palavras personalizadas você quer?: "))
+    dig = int(input("Quantos caracteres você quer?: "))
+    wordlist = []
+    list1 = words[:]  # Usando uma cópia de 'words'
+    list2 = words[:]  # Usando uma cópia de 'words'
+    list1.sort(reverse=True)
+    random.shuffle(list2)  # Embaralhar a lista
+    for w in range(senhas):
+        word = random.choice(list1)
+        while len(word) < dig:
+            word += random.choice(list2)
+        wordlist.append(word)
+    with open("words.txt", "w") as file:
+        file.write("\n".join(wordlist))  # Escrevendo todas as palavras separadas por uma nova linha
